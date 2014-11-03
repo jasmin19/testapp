ecd08fe29e3e3d091d1fe6df16987335979d3acc
jasmin19 <jasmin.wycislok@nexiles.com>
2014-11-03 16:32:03 +0100

    add bunnys

--- src/testapp.coffee
+++ src/testapp.coffee
@@ -1,19 +1,36 @@
 animate = ->
-  requestAnimFrame animate
-  bunny.rotation += 0.1
+    requestAnimFrame animate
+    bunny.rotation += 0.1
+    renderer.render stage
+    return
 
-  # render the stage
-  renderer.render stage
-  return
+get_random_num = (f, t) ->
+    n = Math.floor((Math.random() * t) + f)
+    return n
+
+create_bunny = (x, y, texture) ->
+
+    bunny = new PIXI.Sprite(texture)
+    bunny.anchor.x = 0.5
+    bunny.anchor.y = 0.5
+    bunny.position.x = x
+    bunny.position.y = y
+
+    bunny.interactive = yes
+    bunny.buttonMode = yes
+
+    bunny.click = (interactionData) ->
+        @position.x = @position.x+1
+    return bunny
 
 stage = new PIXI.Stage(0x66ff99)
-renderer = PIXI.autoDetectRenderer(400, 300)
+renderer = PIXI.autoDetectRenderer(800, 800)
 document.body.appendChild renderer.view
 requestAnimFrame animate
+renderer.render stage
 texture = PIXI.Texture.fromImage("bunny.png")
-bunny = new PIXI.Sprite(texture)
-bunny.anchor.x = 0.5
-bunny.anchor.y = 0.5
-bunny.position.x = 200
-bunny.position.y = 150
-stage.addChild bunny
+
+for i in[0..3]
+
+    bunny = create_bunny(get_random_num(200, 250), get_random_num(150, 200), texture)
+    stage.addChild bunny
--- static/testapp.js
+++ static/testapp.js
@@ -1,5 +1,5 @@
 (function() {
-  var animate, bunny, renderer, stage, texture;
+  var animate, bunny, create_bunny, get_random_num, i, renderer, stage, texture, _i;
 
   animate = function() {
     requestAnimFrame(animate);
@@ -7,26 +7,42 @@
     renderer.render(stage);
   };
 
+  get_random_num = function(f, t) {
+    var n;
+    n = Math.floor((Math.random() * t) + f);
+    return n;
+  };
+
+  create_bunny = function(x, y, texture) {
+    var bunny;
+    bunny = new PIXI.Sprite(texture);
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+    bunny.position.x = x;
+    bunny.position.y = y;
+    bunny.interactive = true;
+    bunny.buttonMode = true;
+    bunny.click = function(interactionData) {
+      return this.position.x = this.position.x + 1;
+    };
+    return bunny;
+  };
+
   stage = new PIXI.Stage(0x66ff99);
 
-  renderer = PIXI.autoDetectRenderer(400, 300);
+  renderer = PIXI.autoDetectRenderer(800, 800);
 
   document.body.appendChild(renderer.view);
 
   requestAnimFrame(animate);
 
+  renderer.render(stage);
+
   texture = PIXI.Texture.fromImage("bunny.png");
 
-  bunny = new PIXI.Sprite(texture);
-
-  bunny.anchor.x = 0.5;
-
-  bunny.anchor.y = 0.5;
-
-  bunny.position.x = 200;
-
-  bunny.position.y = 150;
-
-  stage.addChild(bunny);
+  for (i = _i = 0; _i <= 3; i = ++_i) {
+    bunny = create_bunny(get_random_num(200, 250), get_random_num(150, 200), texture);
+    stage.addChild(bunny);
+  }
 
 }).call(this);
